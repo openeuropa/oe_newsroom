089e806189dba2dcf7bb831ed16699a72c3116a4
Andreas Hennings <andreas@dqxtech.net>
2026-02-16 22:21:07 +0100

    Use dependency injection in the hooks class.

--- modules/oe_newsroom_notification_example/src/Hook/NodeNewsroomNotificationHooks.php
+++ modules/oe_newsroom_notification_example/src/Hook/NodeNewsroomNotificationHooks.php
@@ -4,16 +4,31 @@ declare(strict_types=1);
 
 namespace Drupal\oe_newsroom_notification_example\Hook;
 
+use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Utility\Error;
 use Drupal\node\NodeInterface;
 use Drupal\oe_newsroom_newsletter\Api\NewsroomClientInterface;
 use Drupal\oe_newsroom_newsletter\Exception\ClientException;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 /**
  * Hooks to send notifications for node-related events.
  */
 class NodeNewsroomNotificationHooks {
+
+  /**
+   * Constructs a new instance.
+   */
+  public function __construct(
+    protected readonly NewsroomClientInterface $newsroomClient,
+    protected readonly MessengerInterface $messenger,
+    #[Autowire(service: 'logger.channel.oe_newsroom_newsletter')]
+    protected readonly LoggerInterface $logger,
+    protected readonly TimeInterface $time,
+  ) {}
 
   /**
    * Hook_ENTITY_TYPE_insert()
@@ -23,7 +38,7 @@ class NodeNewsroomNotificationHooks {
     // In order to allow users to subscribe, we must create an initial
     // notification.
     try {
-      \Drupal::service(NewsroomClientInterface::class)->nodeNotificationCreate(
+      $this->newsroomClient->nodeNotificationCreate(
         // Section is the identifier for the templates to be used.
         // This needs to be changed based on project configured sections in
         // newsroom.
@@ -33,13 +48,13 @@ class NodeNewsroomNotificationHooks {
         notification_url: $node->toUrl()->setAbsolute()->toString(),
         node_id: $node->id(),
         node_title: $node->getTitle(),
-        create_date: (string) \Drupal::time()->getRequestTime(),
+        create_date: (string) $this->time->getRequestTime(),
       );
-      \Drupal::messenger()->addStatus('Notification created successfully.');
+      $this->messenger->addStatus('Notification created successfully.');
     }
     catch (ClientException $e) {
-      \Drupal::messenger()->addError(t('An error occurred while processing your the notification creation, please check logs to know more about the issue.'));
-      \Drupal::logger('oe_newsroom_newsletter')->error('%type thrown while creating a node notification in %function (line %line of %file).', [] + Error::decodeException($e->getPrevious()));
+      $this->messenger->addError(t('An error occurred while processing your the notification creation, please check logs to know more about the issue.'));
+      $this->logger->error('%type thrown while creating a node notification in %function (line %line of %file).', [] + Error::decodeException($e->getPrevious()));
     }
   }
 
@@ -55,20 +70,20 @@ class NodeNewsroomNotificationHooks {
     }
 
     try {
-      \Drupal::service(NewsroomClientInterface::class)->nodeNotificationCreate(
+      $this->newsroomClient->nodeNotificationCreate(
         section_id: '17965',
         notification_title: sprintf('Updated: %s', $node->getTitle()),
         notification_description: sprintf('The following page has been updated: %s', $node->getTitle()),
         notification_url: $node->toUrl()->setAbsolute()->toString(),
         node_id: $node->id(),
         node_title: $node->getTitle(),
-        create_date: (string) \Drupal::time()->getRequestTime(),
+        create_date: (string) $this->time->getRequestTime(),
       );
-      \Drupal::messenger()->addStatus('Notification created successfully.');
+      $this->messenger->addStatus('Notification created successfully.');
     }
     catch (ClientException $e) {
-      \Drupal::messenger()->addError(t('An error occurred while processing your the notification update, please check logs to know more about the issue.'));
-      \Drupal::logger('oe_newsroom_newsletter')->error('%type thrown while creating a node notification in %function (line %line of %file).', [] + Error::decodeException($e->getPrevious()));
+      $this->messenger->addError(t('An error occurred while processing your the notification update, please check logs to know more about the issue.'));
+      $this->logger->error('%type thrown while creating a node notification in %function (line %line of %file).', [] + Error::decodeException($e->getPrevious()));
     }
   }
 
@@ -78,12 +93,12 @@ class NodeNewsroomNotificationHooks {
   #[Hook('node_delete')]
   public function nodeDelete(NodeInterface $node): void {
     try {
-      \Drupal::service(NewsroomClientInterface::class)->nodeNotificationDelete($node->id());
-      \Drupal::messenger()->addStatus('Notification deleted successfully.');
+      $this->newsroomClient->nodeNotificationDelete($node->id());
+      $this->messenger->addStatus('Notification deleted successfully.');
     }
     catch (ClientException $e) {
-      \Drupal::messenger()->addError(t('An error occurred while processing your the notification deletion, please check logs to know more about the issue.'));
-      \Drupal::logger('oe_newsroom_newsletter')->error('%type thrown while creating a node notification in %function (line %line of %file).', [] + Error::decodeException($e->getPrevious()));
+      $this->messenger->addError(t('An error occurred while processing your the notification deletion, please check logs to know more about the issue.'));
+      $this->logger->error('%type thrown while creating a node notification in %function (line %line of %file).', [] + Error::decodeException($e->getPrevious()));
     }
   }
 
