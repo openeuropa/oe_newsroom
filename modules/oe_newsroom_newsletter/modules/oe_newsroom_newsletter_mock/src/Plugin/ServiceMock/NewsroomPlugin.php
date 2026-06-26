700bb5a8d6f0d3446bfd85244abe0a47fd776687
Andreas Hennings <andreas@dqxtech.net>
2026-06-26 19:43:44 +0200

    D8EMA-5406: Refactor: Use str_starts_with() and add line break.

--- modules/oe_newsroom_newsletter/modules/oe_newsroom_newsletter_mock/src/Plugin/ServiceMock/NewsroomPlugin.php
+++ modules/oe_newsroom_newsletter/modules/oe_newsroom_newsletter_mock/src/Plugin/ServiceMock/NewsroomPlugin.php
@@ -86,7 +86,8 @@ class NewsroomPlugin extends PluginBase implements ServiceMockPluginInterface, C
    * {@inheritdoc}
    */
   public function applies(RequestInterface $request, array $options): bool {
-    return $request->getUri()->getHost() === 'ec.europa.eu' && strpos($request->getUri()->getPath(), '/newsroom/api/v1/') === 0;
+    return $request->getUri()->getHost() === 'ec.europa.eu' &&
+      str_starts_with($request->getUri()->getPath(), '/newsroom/api/v1/');
   }
 
   /**
