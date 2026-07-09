7e86b66f73d857f82eefc6b5b34f9ff23b937afd
Andreas Hennings <andreas@dqxtech.net>
2026-07-09 16:08:33 +0200

    DISCARD: Add new endpoint methods in NewsroomClient.

--- modules/oe_newsroom_newsletter/src/Api/NewsroomClientInterface.php
+++ modules/oe_newsroom_newsletter/src/Api/NewsroomClientInterface.php
@@ -97,4 +97,107 @@ interface NewsroomClientInterface {
    */
   public function unsubscribe(string $email, array $svIds = []): bool;
 
+  /**
+   * Gets mail subscriptions.
+   *
+   * @param string $email
+   *   The email to obtain the subscriptions from.
+   *
+   * @return array
+   *   The subscriptions list.
+   */
+  public function subscriptions(string $email): array;
+
+  /**
+   * Create node notification.
+   *
+   * @param string $section_id
+   *   The section.
+   * @param string $notification_title
+   *   The notfication title.
+   * @param string $notification_description
+   *   The notfication description.
+   * @param string $notification_url
+   *   The notfication URL.
+   * @param string $node_id
+   *   The node ID.
+   * @param string $node_title
+   *   The node title.
+   * @param string $create_date
+   *   The creation date.
+   */
+  public function nodeNotificationCreate(
+    string $section_id,
+    string $notification_title,
+    string $notification_description,
+    string $notification_url,
+    string $node_id,
+    string $node_title,
+    // Is this required and what is the expected format?
+    string $create_date,
+  ): void;
+
+  /**
+   * Delete node notification.
+   *
+   * @param string $node_id
+   *   The node ID.
+   */
+  public function nodeNotificationDelete(string $node_id): void;
+
+  /**
+   * Subscribe an e-mail to a node notification.
+   *
+   * @todo For the moment keep this as a separate method since method signature
+   *   is quite different. Can we join both?
+   *
+   * @param string $node_id
+   *   The node ID.
+   * @param string $email
+   *   The user email.
+   * @param int $frequency
+   *   The subscription frequency.
+   * @param bool $nomail
+   *   If the subscription require confirmation.
+   *
+   * @return array
+   *   The node subscription details.
+   */
+  public function nodeNotificationSubscribe(
+    string $node_id,
+    string $email,
+    int $frequency,
+    bool $nomail = TRUE,
+  ): array;
+
+  /**
+   * Unsubscribe from a node notification.
+   *
+   * @param string $node_id
+   *   The node ID.
+   * @param string $email
+   *   The user email.
+   * @param bool $request_authentication
+   *   If the user is requested to authenticate.
+   * @param string|null $redirect_to
+   *   The URL to redirect after the unsubcription.
+   */
+  public function nodeNotificationUnsubscribe(
+    string $node_id,
+    string $email,
+    bool $request_authentication = FALSE,
+    ?string $redirect_to = '',
+  ): void;
+
+  /**
+   * Check if a node is present in Newsroom.
+   *
+   * @param string $node_id
+   *   The node ID.
+   *
+   * @return array
+   *   Decoded response data.
+   */
+  public function nodeNotificationGet(string $node_id): array;
+
 }
