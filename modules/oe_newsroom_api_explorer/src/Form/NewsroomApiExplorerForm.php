b5c92d201ffa4ec2b9c63912e123076ea9d00324
Andreas Hennings <andreas@dqxtech.net>
2026-09-09 19:40:17 +0200

    D8EMA-5353: Testing: Expose the VcrStore class in the API explorer.

--- modules/oe_newsroom_api_explorer/src/Form/NewsroomApiExplorerForm.php
+++ modules/oe_newsroom_api_explorer/src/Form/NewsroomApiExplorerForm.php
@@ -25,6 +25,7 @@ use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\oe_newsroom\Endpoint\NodeNotificationEndpoints;
 use Drupal\oe_newsroom_api_explorer\Helper\ReflectionHelper;
 use Drupal\oe_newsroom_newsletter\Api\NewsroomClient;
+use Drupal\oe_newsroom_vcr\Vcr\VcrStore;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Promise\Create;
 use GuzzleHttp\Promise\PromiseInterface;
@@ -595,6 +596,8 @@ class NewsroomApiExplorerForm implements FormInterface, ContainerInjectionInterf
     $classes = [
       NewsroomClient::class,
       NodeNotificationEndpoints::class,
+      // Allow developers to play with the VCR in the API explorer.
+      VcrStore::class,
     ];
     $options = [];
     foreach ($classes as $class) {
